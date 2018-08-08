b51687913109e1b9750682265d4a443047467f9e
Petr Vaněk <arkamar@atlas.cz>
2018-08-08 20:16:06 +0200

    log: open unbuffered file for appending

--- log.c
+++ log.c
@@ -8,17 +8,25 @@ static FILE * stream;
 static
 void __attribute__((constructor))
 init() {
+	stream = fopen("/tmp/koku.dbg", "a");
+	if (stream)
+		setvbuf(stream, NULL, _IONBF, 0);
 }
 
 static
 void __attribute__((destructor))
 fini() {
+	if (stream)
+		fclose(stream);
 }
 
 int
 koku_log(const char * format, ...) {
 	int ret;
 	va_list args;
+
+	if (!stream)
+		return 0;
 
 	va_start(args, format);
 	ret = vfprintf(stream, format, args);
